8f5b42b1870657318ecdfd88ed849b2d9725ef49
jackzheng2496 <jackzheng2496@gmail.com>
2017-05-15 16:33:37 -0400

    scoreboard in the middle

--- src/random.c
+++ src/random.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+int gameState;
+int score;
+int acceleration;
+
 enum ORIENTATION { RIGHT, LEFT, UP, DOWN };
 
 struct window {
@@ -22,7 +26,7 @@ void create_win(struct window *window)
 {
     window->win = newwin(window->height, window->width,
                          window->starty, window->startx);
-    box(window->win, 0, 0);                              
+    box(window->win, window->startx, window->starty);                              
     wrefresh(window->win);
 }
 
@@ -51,9 +55,7 @@ void printPaddle(struct window *window, struct paddle *pd)
 
 int collision(struct pong *p, struct paddle *pd)
 {
-  if (p->x == 3 && (p->y >= pd->top && p->y <= pd->bottom))
-    return 1;
-  return 0;
+  return p->x == 3 && (p->y >= pd->top && p->y <= pd->bottom);
 }
 
 void *renderBall(void *vargs)
@@ -66,19 +68,32 @@ void *renderBall(void *vargs)
   while (1) {
 
     // TODO: Collision detection here
+    // Ball movement should behave differently if it hits paddle
+    if (collision(p,pd)) {
+      p->o_rl = (p->o_rl == RIGHT) ? LEFT : RIGHT;
+      if (++score % 2 == 0)
+        acceleration -= 10;
+    }
 
-    if (collision(p, pd) || p->x == (win->width-1) || p->x == 0) {
-        p->o_rl = (p->o_rl == RIGHT) ? LEFT : RIGHT;
+    // Did not hit paddle, end game
+    if (p->x == 0) {
+      gameState = 0;
+      break;
+    }
+
+    if (p->x == (win->width-1)) {
+      p->o_rl = (p->o_rl == RIGHT) ? LEFT : RIGHT;
     }
 
     if (p->y == (win->height-1) || p->y == 0) {
-        p->o_ud = (p->o_ud == UP) ? DOWN : UP;
+      p->o_ud = (p->o_ud == UP) ? DOWN : UP;
     }
 
     p->x += (p->o_rl == RIGHT) ? 1 : -1;
     p->y += (p->o_ud == UP) ? -1 : 1;
 
-    napms(50);
+    // Control how fast the ball moves
+    napms(acceleration);
   }
 
   return NULL;
@@ -88,6 +103,10 @@ int main()
 {
     int ch;
     int mx, my;
+    char c;
+    pthread_t ball;
+    WINDOW *my_win;
+    WINDOW *score_win;
     /*
      * Initialization
      */
@@ -100,43 +119,46 @@ int main()
 
     /* Find boundaries of the screen */
     getmaxyx(stdscr, my, mx);
-    
-    WINDOW *my_win;
 
-    struct render r;
-    struct window win;
+    struct window win = {
+      .height = my-2,
+      .width = mx-2,
+      .starty = 1,
+      .startx = 1
+    };
 
-    win.height = my - 2;
-    win.width = mx - 2;
-    win.starty = 1;
-    win.startx = 1;
+    struct pong p = {
+      .x = mx>>1,
+      .y = (my>>1)-1,
+      .o_rl = RIGHT,
+      .o_ud = DOWN
+    };
 
-    struct pong p;
-    struct paddle pd = {-3+my/2, 3+my/2};
+    struct paddle pd = {
+      .top = -3+(my>>1), 
+      .bottom = 3+(my>>1)
+    };
 
-    p.x = mx / 2;
-    p.y = my / 2 - 1;
+    struct render r = {
+      .p = &p,
+      .w = &win,
+      .pd = &pd
+    };
 
-    p.o_rl = RIGHT;
-    p.o_ud = DOWN;
-
+    acceleration = 50;
+    gameState = 1;
+    c = 'N';
     /*
      * well, this is suppose to remove cursor, 
      * but functionality is not available on my terminal :\
      */
     curs_set(0);
     create_win(&win);
-    //cbreak();
-    //nodelay(win.win,0); 
 
     // Create another thread for rendering ball movement
-    r.p = &p;
-    r.w = &win;
-    r.pd = &pd;
-    pthread_t ball;
     pthread_create(&ball, NULL, renderBall, (void*)&r);
-    char c = 'N';
-    while (1) {
+
+    while (gameState) {
         c = getch();
 // Co-creator - Norman Le
         switch (c) {
@@ -158,11 +180,18 @@ int main()
        
         wclear(win.win);
         mvwprintw(win.win, p.y, p.x, "%c", 'O');
+        mvwprintw(win.win, 2, -3+(mx >> 1), "Score : %d", score);
         printPaddle(&win, &pd);
         wborder(win.win, '|','|','-','-','+','+','+','+');
         wrefresh(win.win);
     }
     pthread_join(ball,NULL); 
+    wclear(win.win);
+    mvwprintw(win.win, 1, 0, "Final Score : %d", score);
+    mvwprintw(win.win, 0, 0, "Game Over (press 'q' to quit)");
+    wrefresh(win.win);
+    while ((c = getch()) != 'q')
+      ;
     endwin();
     return 0;
 }
