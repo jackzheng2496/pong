480df2be4e57b43784c38a93835ffbd2e7520ec4
jackzheng2496 <jackzheng2496@gmail.com>
2017-05-26 14:44:27 -0400

    starting some stuff for shitty ai paddle movement

--- src/main.c
+++ src/main.c
@@ -4,10 +4,26 @@ int gameState;
 int score;
 int acceleration;
 
+struct ai {
+  int ai_y;
+  struct window *win;
+  struct pong *p;
+};
+
+void *renderAI(void *vargs)
+{
+  struct ai *p_ai = (struct ai *) vargs;
+  while (gameState) {
+    p_ai->ai_y = p_ai->p->y;
+  }
+  return NULL;
+}
+
 int main()
 {
     int ch;
     int mx, my;
+    int ai_y;
     char c;
     pthread_t ball;
     WINDOW *my_win;
@@ -39,6 +55,8 @@ int main()
       .o_ud = DOWN
     };
 
+    ai_y = (my>>1)-1;
+
     struct paddle pd = {
       .top = -3+(my>>1), 
       .bottom = 3+(my>>1)
@@ -63,6 +81,16 @@ int main()
     // Create another thread for rendering ball movement
     pthread_create(&ball, NULL, renderBall, (void*)&r);
 
+    // Create another thread for opponent 'AI' paddle
+    pthread_t ai;
+    struct ai p_ai;
+
+    p_ai.ai_y = ai_y;
+    p_ai.win = &win;
+    p_ai.p = &p;
+
+    pthread_create(&ai, NULL, renderAI, (void*)&p_ai);
+
     while (gameState) {
         c = getch();
 // Co-creator - Norman Le
@@ -82,15 +110,26 @@ int main()
         default:
           break;
         }
-       
+        
+        // Rendering pong ball
         wclear(win.win);
         mvwprintw(win.win, p.y, p.x, "%c", 'O');
         mvwprintw(win.win, 2, -3+(mx >> 1), "Score : %d", score);
+
+        // Rendering user paddle
         printPaddle(&win, &pd);
+
+        // Rendering AI paddle
+        mvwprintw(win.win, p_ai.ai_y, mx-5, "%c", 'P');
+
+        // Rendering window
         wborder(win.win, '|','|','-','-','+','+','+','+');
+
+        // Refresh window to get new frame
         wrefresh(win.win);
     }
     pthread_join(ball,NULL); 
+
     wclear(win.win);
     mvwprintw(win.win, 1, 0, "Final Score : %d", score);
     mvwprintw(win.win, 0, 0, "Game Over (press 'q' to quit)");
